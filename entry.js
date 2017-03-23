c7c644d4ff09bd342c91beeff09fdc6ab2f6e300
hutingting119 <1912257350@qq.com>
2017-03-23 23:06:10 +0800

    first comit

---
+++ entry.js
@@ -0,0 +1,10 @@
+
+require("./style.css");
+import React, {Component} from 'react';
+import {render} from "react-dom";
+class HelloMessage extends React.Component {
+    render() {
+        return <div>Hello</div>;
+    }
+}
+render(<HelloMessage/>, document.getElementById('app'));
